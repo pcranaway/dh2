11d2c015e8d8cfd829b8653b2e1c200cdd5406eb
pcranaway <pcranaway@tuta.io>
2021-08-20 01:56:26 +0300

    add start method

--- server.ts
+++ server.ts
@@ -3,9 +3,24 @@ export class Server {
     host: string;
     port: number;
 
+    listener?: Deno.Listener;
+
     constructor(host: string = '0.0.0.0', port: number = 8080) {
         this.host = host;
         this.port = port;
+    }
+
+    async start() {
+        this.listener = Deno.listen({
+            hostname: this.host,
+            port: this.port
+        });
+
+        while(true) {
+            const connection = await this.listener.accept();
+            
+            // handle connection
+        }
     }
 
 }
